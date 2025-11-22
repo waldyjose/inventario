034b8288456ea8c8a5bf3ed684ecdce118e35f41
waldy jose <waldyjosede@gmail.com>
2025-11-22 11:12:01 -0500

    Connecting DB - HTTP:GET

--- src/controllers/categoria.controllers.js
+++ src/controllers/categoria.controllers.js
@@ -1,5 +1,14 @@
-const getCategorias =(req, res)=>{
-    res.json({"Categoria":"Electrodomesticos"})
+import getConnection from "./../db/database.js"
+
+const getCategorias =async (req, res)=>{
+    try {
+        const connection = await getConnection();
+        const result = await connection.query("SELECT CategoriaID, CategoriaNombre, Descripcion, Imagen FROM categorias")
+        res.json(result);
+    } catch (error) {
+     console.error("ERROR 404");   
+    }
+    
 }
 
 export const methodHTTP = {
